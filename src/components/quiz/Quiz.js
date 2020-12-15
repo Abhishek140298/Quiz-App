01ba763275243405d7d8ff3fa78453572a421e65
Abhishek <abhishekyadavfeb1498@gmail.com>
2020-12-15 08:44:55 +0530

    css added

--- src/components/quiz/Quiz.js
+++ src/components/quiz/Quiz.js
@@ -6,7 +6,9 @@ const option = ['a) E=m2c', 'b) E=mc2', 'c) E=mc', 'd) E=m/c'];
 const Quiz = () => {
   return (
     <div className='quiz_box'>
-      <h2>SCIENCE QUIZ</h2>
+      <h2 className='h2_heading'>SCIENCE QUIZ</h2>
+      <div className='image_box'>
+      <img className='question_image' src='https://thumbs.dreamstime.com/z/surprise-albert-einstein-stand-question-mark-136009956.jpg'/>
       <div className='question_box'>
       <span className='question'>
         What is the Einten's energy conservation ?
@@ -14,6 +16,7 @@ const Quiz = () => {
       {option.map((cval) => {
         return <span className='options'>{cval}</span>;
       })}
+</div>
 </div>
 <div className='response'>
 <span className='submit_button'>SUBMIT</span>
