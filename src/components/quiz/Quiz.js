6b2d7c69d854392120ec06e21ac726131ff229ef
Abhishek <abhishekyadavfeb1498@gmail.com>
2020-12-20 23:06:45 +0530

    option selection is set

--- src/components/quiz/Quiz.js
+++ src/components/quiz/Quiz.js
@@ -3,10 +3,10 @@ import './Quiz.css';
 import { connect } from 'react-redux';
 import { getQuizData } from '../../action/QuizDataAction';
 
-const option = ['a) E=m2c', 'b) E=mc2', 'c) E=mc', 'd) E=m/c'];
-
 const Quiz = (props) => {
   const [currentPage, setCurrentPage] = useState(0);
+  const [optionColor, setOptionColor] = useState('');
+  const [spanId, setSpanId] = useState();
 
   useEffect(() => {
     props.getQuizData();
@@ -14,18 +14,21 @@ const Quiz = (props) => {
   const perPage = 1;
   const offset = perPage * currentPage;
   const questions = props.quizData[props.match.params.id].q_a;
-  console.log('question', questions);
+
   const changeCurrentPage = () => {
     if (currentPage < 9) {
       return setCurrentPage(currentPage + 1);
-    } 
-  };
-  const reduceCurrentPage=()=>{
-    if(currentPage > 0){
-      setCurrentPage(currentPage - 1)
     }
-  }
-  console.log("currentpage",currentPage);
+  };
+  const reduceCurrentPage = () => {
+    if (currentPage > 0) {
+      return setCurrentPage(currentPage - 1);
+    }
+  };
+  const optionSelect = (e) => {
+    return setOptionColor('#008000	'), setSpanId(e.target.id);
+  };
+
   return (
     <div className='quiz_box'>
       <h2 className='h2_heading'>SCIENCE QUIZ</h2>
@@ -38,10 +41,20 @@ const Quiz = (props) => {
           {questions.slice(offset, offset + perPage).map((cval) => {
             return (
               <>
-                {' '}
                 <span className='question'>{cval.question}</span>
-                {cval.options.map((s_cval) => {
-                  return <span className='options'>{s_cval}</span>;
+                {cval.options.map((s_cval, index) => {
+                  return (
+                    <span
+                      className='options'
+                      id={index}
+                      style={
+                        spanId== index ? { backgroundColor: optionColor } : null
+                      }
+                      onClick={optionSelect}
+                    >
+                      {s_cval}
+                    </span>
+                  );
                 })}
               </>
             );
@@ -50,7 +63,9 @@ const Quiz = (props) => {
       </div>
       <div className='response'>
         <span className='submit_button'>SUBMIT</span>
-        <span className='cancel_button' onClick={reduceCurrentPage}>CANCEL</span>
+        <span className='cancel_button' onClick={reduceCurrentPage}>
+          CANCEL
+        </span>
         <span className='skip_button' onClick={changeCurrentPage}>
           SKIP
         </span>
